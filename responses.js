85c057b29350e98ad06f4afb84d0ce3e4fc2bea6
sasidhar2525709 <106161539+sasidhar2525709@users.noreply.github.com>
2022-05-31 01:27:36 -0700

    Add files via upload

---
+++ responses.js
@@ -0,0 +1,71 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock")  {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+    
+
+    // Simple responses
+    if (input == "who is your boss") {
+        return "MR Chandu";
+       
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+         } else if (input == "") {
+        return "Talk  later!";
+         } else if (input == "who created you") {
+        return "chandu sir created me";
+          } else if (input == "hello") {
+        return "hello how can i help you?";
+          } else if (input == "your ceo") {
+        return "chandu sir is ceo of mt travels";
+  } else if (input == "who is your ceo") {
+        return "chandu sir is ceo of mt travels";
+  } else if (input == "your ceo fav hero") {
+        return " ram charan";
+      } else if (input == "hii macha") {
+        return " hii ra mama em pani"; 
+ } else if (input == "hi macha") {
+        return " hii ra mama em pani"; 
+ } else if (input == "hii mama") {
+        return " hii macha em katha"; 
+         } else if (input == "hi mama") {
+        return " hii macha em katha"; 
+ } else if (input == "ram charan new movie") {
+        return "RRR"; 
+         } else if (input == "fuck u") {
+        return " na love da";
+         } else if (input == "hoo") {
+        return "hii how can i help you";
+
+    } else if (input == "hi") {
+        return "hii how can i help you";
+            } else if (input == "i love you") {
+        return "i love you to";
+    } else if (input == "can i speak to you") {
+        return "offcouse sir 9550500678 call me";
+
+    } else if (input == "iam getting issue in booking") {
+        return "sorry for your inconvenence sir we will help you please provide your mobile number";
+    } else if (input == "what is your name") {
+        return "aggi petti macha";
+    } else if (input == "rey") {
+        return "aa nakodaka";
+        } else if (input == "hoo") {
+        return "aa nakodak"; 
+
+  
+
+
+
+
+    } else {
+        return "Try asking something else!";
+    }
+   
+    
+}
